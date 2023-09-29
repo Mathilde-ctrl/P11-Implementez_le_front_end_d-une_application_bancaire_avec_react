ef21cd2dfcebaad1d56ea78e23c5db2973b6a91a
Mathilde Plancq <mathilde.plancq@gmail.com>
2023-09-29 15:38:04 +0200

    Change UserName functionality

--- ArgentBank-website-main/FRONTEND/argent-bank/src/components/form/form.jsx
+++ ArgentBank-website-main/FRONTEND/argent-bank/src/components/form/form.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import { useDispatch } from "react-redux";
-import { getloginUser } from '../../redux/reducers/test';
+import { getloginUser } from '../../redux/reducers/authSlice';
 import { useNavigate } from 'react-router-dom';
 import store from '../../redux/store'
 
--- ArgentBank-website-main/FRONTEND/argent-bank/src/pages/user/user.jsx
+++ ArgentBank-website-main/FRONTEND/argent-bank/src/pages/user/user.jsx
@@ -3,29 +3,66 @@ import Nav from "../../components/nav/nav";
 import Footer from "../../components/footer/footer";
 import Welcome from "../../components/welcome/welcome";
 import Account from "../../components/account/account";
+import Edit from '../../components/edit_user/edit_user'
+
+import { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { changeName } from "../../redux/reducers/userEditSlice";
+import { loginSuccess } from "../../redux/reducers/authSlice";
+
 
 function User(){
-  // This userName is going away if refresh because not stored. 
-  // const userName = useSelector((state) => state.auth.userName)
+  const dispatch = useDispatch();
+  const userName = useSelector((state) => state.auth.userName)
+  const firstName = useSelector((state) => state.auth.firstName )
+  const lastName = useSelector((state) => state.auth.lastName )
+  console.log("userName : ", userName) 
+  /**
+   * @variable isEditing - with initial value set to 'false'
+   * @function setIsEditing - update the state 
+   */
+  const [isEditing, setIsEditing] = useState(false);   
 
-
-  // const profileData = useSelector((state) => state.auth.body)
-  //console.log('*****' + profileData.profileData)
-  // const userName = profileData ? profileData.userName : "No"
-
-  // If page is refresh the userName from the state is gone unless I store 
-  // the state inside a sessionStorage. 
-  const userName = sessionStorage.getItem('userName')
+  /**
+   * @function toggleEditing - Responsible for switching the Welcome and Edit components 
+   * with onClick on buttons. 
+   */
+  function toggleEditing(){
+    setIsEditing(!isEditing);
+  }
 
   const token = sessionStorage.getItem('token');
   console.log("token : ", token)
 
+  // remove Token from the sessionStorage to clean it when a user logout. (console/Application/sessionStorage)
   function SignOut(){
     sessionStorage.removeItem('token')
-    sessionStorage.removeItem('userName')
   }
 
-  
+  //
+
+  const handleSaveUserName = (newUserName) => {
+    try {
+      const response = dispatch(changeName(newUserName));
+      const updatedUserName = response.payload;
+      dispatch(loginSuccess({ userName: updatedUserName, firstName, lastName }));
+      toggleEditing();
+    } catch (error) {
+      console.log('Error while updating userName: ', error);
+    }
+  }
+
+/**
+  const handleSaveUserName = (newUserName) => {
+    try{
+      dispatch(changeUserName(newUserName));
+      toggleEditing();
+    }catch(error){
+      console.log('Error while updating userName : ', error)
+    }
+  }
+   */
+   // ----------------------------------------------------
 
   return(
     <div> 
@@ -38,12 +75,21 @@ function User(){
         Icon="fa fa-sign-out"
         Text="Sign out"
         Function={SignOut}
-        
+         
       />
       <main className="main bg-dark">
-        <Welcome 
-          Name={userName}
-        />
+        {!isEditing ? (
+          <Welcome Name={userName} onEditClick={toggleEditing} />
+        ) : (
+          <Edit 
+            cancelClick={toggleEditing} 
+            saveClick={handleSaveUserName}
+            firstName={firstName}
+            lastName={lastName}
+            
+          />
+        )}
+               
         <h2 className="sr-only">Accounts</h2>
         <Account 
           Title="Argent Bank Checking"
